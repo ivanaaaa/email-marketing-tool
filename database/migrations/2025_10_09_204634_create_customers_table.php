bf5df710a047307fac5a1fddd9ef998f835609d7
Ivana <ivanacebova@gmail.com>
2025-10-09 22:55:11 +0200

    Add customers table migration -option sex and birth_date fields -foreign key user_id - unique constraint on user_id and email - index on email

---
+++ database/migrations/2025_10_09_204634_create_customers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->string('email')->index();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->enum('sex', ['male', 'female', 'other'])->nullable();
+            $table->date('birth_date')->nullable();
+            $table->timestamps();
+
+            $table->unique(['user_id', 'email']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
