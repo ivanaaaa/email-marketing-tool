ebe1d7b617ab00dcd194cebecd8042d9b7b0af1a
Ivana <ivanacebova@gmail.com>
2025-10-10 01:25:02 +0200

    add notification for sending emails, use queue

---
+++ app/Notifications/CampaignEmailNotification.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class CampaignEmailNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+
+    private string $body;
+    private string $subject;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(string $subject, string $body)
+    {
+        $this->subject = $subject;
+        $this->body = $body;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject($this->subject)
+            ->line($this->body);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(object $notifiable): array
+    {
+        return [
+            'subject' => $this->subject,
+            'body' => $this->body,
+        ];
+    }
+}
