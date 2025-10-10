d7194f4c77bba2021796a95595e67899c74ef98b
Ivana <ivanacebova@gmail.com>
2025-10-10 10:30:53 +0200

    register policies

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,15 @@
 
 namespace App\Providers;
 
+use App\Models\Campaign;
+use App\Models\Customer;
+use App\Models\EmailTemplate;
+use App\Models\Group;
+use App\Policies\CampaignPolicy;
+use App\Policies\CustomerPolicy;
+use App\Policies\EmailTemplatePolicy;
+use App\Policies\GroupPolicy;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +28,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Register policies
+        Gate::policy(Customer::class, CustomerPolicy::class);
+        Gate::policy(Group::class, GroupPolicy::class);
+        Gate::policy(EmailTemplate::class, EmailTemplatePolicy::class);
+        Gate::policy(Campaign::class, CampaignPolicy::class);
     }
 }
