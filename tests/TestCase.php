d3e71577703a97af4097ebbbc3b613f397feac98
Ivana <ivanacebova@gmail.com>
2025-10-30 09:51:44 +0100

    update TestCase.php (to check verified commit)

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,21 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    /**
+     * Setup the test environment.
+     */
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        // Disable CSRF middleware for tests
+        $this->withoutMiddleware([
+            VerifyCsrfToken::class,
+        ]);
+    }
 }
